ad0e791ff0babaea98d31b83dd112967da3488f5
Saakian <avsaakian@gmail.com>
2021-01-29 12:04:51 +0200

    Added Check Mobile categoty to Arxikh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainVideo from "./components/MainVideo";
+import Categories from "../src/components/Category_Mobile";
 import "./components/mainVideo.css";
 import Slider from "../src/components/Slider";
 import Route1 from "../src/components/RouteTest";
@@ -16,6 +17,7 @@ function App() {
           <Route exact path='/Υπηρεσίες' component={Route1} />
           <Route exact path='/route2' component={Route2} />
         </Switch>
+        <Categories />
       </div>
     </Router>
   );

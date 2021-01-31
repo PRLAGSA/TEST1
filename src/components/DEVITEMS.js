152f9ec9005f16ad1ae732f4d3fa774ada036c2b
Saakian <avsaakian@gmail.com>
2021-01-31 12:58:14 +0200

    Responsive developing items DevItems

---
+++ src/components/DEVITEMS.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function DEVITEMS() {
+  return (
+    <div>
+      <div className='MainContDivItems'>
+        <div className='ContDivItems'>
+          <div className='HolderDivItems1'>
+            <ul>
+              <div className='forWrap'>
+                <li>Κατασκευή ιστοσελίδων</li>
+                <li>Κατασκευή E-Shop</li>
+                <li>Φιλοξενία Ιστοσελίδων</li>
+                <li>Κατοχύρωση Domain</li>
+              </div>
+              <div className='forWrap'>
+                <li>Custom Εφαρμογές</li>
+                <li>WordPress Development</li>
+                <li>Laravel Development</li>
+                <li>PHP Development</li>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DEVITEMS;
--- src/components/DevelopmentLink.js
+++ src/components/DevelopmentLink.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import "./DevelopmentLink.css";
-
+import DevItems from "../components/DEVITEMS";
+import "./DEVITEMS.css";
 // import { useHistory } from "react-router-dom";
 
 function DevelopmentLink() {
@@ -10,22 +11,25 @@ function DevelopmentLink() {
   //   history.goBack();
   // };
   return (
-    <div className='text_wrapper'>
-      <h1 className='text'>
-        <div className='imageMac'></div>
-        <div className='textmain1'>
-          16 χρόνια κατασκευάζουμε <br></br>και υποστηρίζουμε εκατοντάδες
-          e-shop,
-          <br></br> με πολλά από αυτά με πάνω από 1.000 <br></br>
-          παραγγελίες την ημέρα.
-        </div>
+    <>
+      <DevItems />
+      <div className='text_wrapper'>
+        <h1 className='text'>
+          <div className='imageMac'></div>
+          <div className='textmain1'>
+            16 χρόνια κατασκευάζουμε <br></br>και υποστηρίζουμε εκατοντάδες
+            e-shop,
+            <br></br> με πολλά από αυτά με πάνω από 1.000 <br></br>
+            παραγγελίες την ημέρα.
+          </div>
 
-        <div className='text2'>E-SHOPS</div>
-      </h1>
+          <div className='text2'>E-SHOPS</div>
+        </h1>
 
-      {/* 
+        {/* 
       <button onClick={goToPreviousPath}>Back</button> */}
-    </div>
+      </div>
+    </>
   );
 }
 

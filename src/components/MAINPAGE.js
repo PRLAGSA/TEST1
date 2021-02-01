7eafaeefdf50952b1302f91a5c2db3839bda5b87
Saakian <avsaakian@gmail.com>
2021-02-01 04:09:38 +0200

    Responsive Banner Gnoriste tin NetMedia

--- src/components/MAINPAGE.js
+++ src/components/MAINPAGE.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 import "./MAINPAGE.css";
 
 function MAINPAGE() {
   return (
     <div>
-      <Link to='/κατασκευη Ιστοσελιδων'>
-        <div className='Banner_Container'>
-          <div className='Banner_Title'>
-            {/* <img src={Banner} alt='banner' className='Banner' /> */}
-            <div className='zIndexContainer'>
-              <div className='threeItems'>
-                <div className='phone'>yyy</div>
-                <div className='tablet'>yyy</div>
-                <div className='iMac'>yyy</div>
-              </div>
+      {/* <Link to='/κατασκευη Ιστοσελιδων'> */}
+      <div className='Banner_Container'>
+        <div className='Banner_Title'>
+          {/* <img src={Banner} alt='banner' className='Banner' /> */}
+          <div className='zIndexContainer'>
+            <h1 className='h1GNORISTE'>Γνωριστε την NETSTUDIO</h1>
+            {/* <h4 className='h4GNORISTE'>
+              Γνωριστε την <bold>NETSTUDIO</bold>
+            </h4> */}
+            <div className='threeItems'>
+              <div className='phone'></div>
+              <div className='tablet'></div>
+              <div className='iMac'></div>
             </div>
           </div>
         </div>
-      </Link>
+        <h4>4985840584059840589489</h4>
+      </div>
+      {/* </Link> */}
     </div>
   );
 }
